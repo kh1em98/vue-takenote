ce9ed77bbd6176f0dabeb57ad464b33d78401923
Khiem Nguyen <khiemalbavds@gmail.com>
2020-07-21 14:26:07 +0700

    Buon ba

--- server/index.js
+++ server/index.js
@@ -35,6 +35,7 @@ app.use(function (req, res) {
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(__dirname + '/public'));
+    app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
 }
 
 
